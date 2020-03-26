938fe512b66e51231ec96c3cc2fd97ebfdea1538
Unisse Chua <unissechua@users.noreply.github.com>
2020-03-26 22:53:46 +0800

    initial commit

---
+++ model/student.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const databaseURL = 'mongodb://localhost:27017/studentsdb';
+const options = { useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false };
+
+mongoose.connect(databaseURL, options);
+
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: [true, "No name provided"] },
+    id: { type: String, required: [true, "No ID number provided"] },
+    img: { type: String, required: true }
+  }
+  // }, {
+  //   toObject: {
+  //     virtuals: true,
+  //   },
+  //   toJSON: {
+  //     virtuals: true,
+  //   }
+  // }
+);
+
+module.exports = mongoose.model('students', studentSchema);
