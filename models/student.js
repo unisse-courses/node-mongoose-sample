336ed9f74196a1d40f4f845615b26cd6f8088095
Unisse Chua <unissechua@users.noreply.github.com>
2020-03-26 23:23:14 +0800

    Add comments

--- models/student.js
+++ models/student.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const databaseURL = 'mongodb://localhost:27017/studentsdb';
+
+/** README **
+  We need to set useFindAndModify to false because mongoose's findOneAndUpdate
+  is using a deprecated function: findAndModify.
+  This will suppress the warning.
+**/
 const options = { useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false };
@@ -12,6 +18,12 @@ const studentSchema = new mongoose.Schema({
     id: { type: String, required: [true, "No ID number provided"] },
     img: { type: String, required: true }
   }
+  /** README **
+    Virtuals are other fields that do not persist in mongodb.
+    By setting virtuals: true for toObject and toJSON, this makes all the
+    Document.toObject() function include any virtuals value available.
+    For our case, we don't have any.
+  **/
   // }, {
   //   toObject: {
   //     virtuals: true,
@@ -22,4 +34,7 @@ const studentSchema = new mongoose.Schema({
   // }
 );
 
+/** README **
+  Export the model as the main content of this module.
+**/
 module.exports = mongoose.model('students', studentSchema);
